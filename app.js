7cee25cc523ac47c00d7ef5c1616f7a2fb96d011
yishaistern <yishainstern@gmail.com>
2019-10-03 17:15:43 +0300

    =aa

---
+++ app.js
@@ -0,0 +1,55 @@
+/*test.js file*/
+const express = require('express');
+const bodyPars = require ('body-parser');
+console.log("Node is working");
+const listener = express();
+var fs = require('fs');
+listener.use(bodyPars());
+
+
+listener.post('/login' , function(request, response) {
+	const body = request.body;
+	fs.readFile('./db/users.json', function(err, data) {
+	    let users = JSON.parse(data);
+	});
+});
+listener.post('/singUp' , function(request, response) {
+	const body = request.body;
+    fs.readFile('./db/users.json', function(err, data) {
+	    let users = JSON.parse(data);
+	    const withName = users.filter((value) => value.userName === body.userName);
+	    console.log(withName);
+    	if (withName && withName.length > 0) {
+    		response.send({
+    			status: {
+    				code: -1,
+    				state: "fail"
+    			},
+    			error : {
+    				msg: "user already exsites"
+    			},
+    			data: {
+
+    			}
+    		});
+    	} else {
+    		users.push(body);
+    		fs.writeFile('./db/users.json', JSON.stringify(users), function (err) {
+  				if (err) throw err;
+  				console.log('Replaced!');
+			});
+    		response.send({
+    			status: {
+    				code: 0,
+    				state: "succsess"
+    			},
+    			
+    			data: body
+    		});
+    	}
+  	});
+    
+});
+listener.listen(4005, function() {
+    console.log('listening at 4005');
+});
